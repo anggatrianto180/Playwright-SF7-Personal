1d3d107983a5b0d896dcae25a46b9836d2ec98df
Angga <anggatrianto180@gmail.com>
2025-07-03 10:28:42 +0700

    update script login

--- tests/Record-Attendance/RecordIn.spec.ts
+++ tests/Record-Attendance/RecordIn.spec.ts
@@ -1,8 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('Record Attendance', async ({ page }) => {
-  await page.goto('https://sfworkplaze.dataon.com/standard/home');
-  await page.locator('[class="ant-btn ant-btn-primary"]').click();
-  await page.locator('[class="ant-typography"]');
-  await expect(page.locator('[class="ant-typography"]').nth(1)).toContainText('Completed');
+    await page.goto('https://sfworkplaze.dataon.com/');
+    await page.waitForTimeout(5000);
+    await expect(page).toHaveTitle(/Human Capital Management System/);
+    await page.getByRole('textbox', { name: '* Username' }).fill('gordon');
+    await page.getByRole('textbox', { name: '* Password' }).fill('password123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForTimeout(5000);
+    await expect(page).toHaveURL('https://sfworkplaze.dataon.com/standard/home');
+    await page.waitForTimeout(5000);
+    await page.locator('[class="ant-btn ant-btn-primary"]').click();
+    await page.locator('[class="ant-typography"]');
+    await expect(page.locator('[class="ant-typography"]').nth(1)).toContainText('Completed');
 });
--- tests/Record-Attendance/addLeave.spec.ts
+++ tests/Record-Attendance/addLeave.spec.ts
@@ -1,5 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('Add Leave Request', async ({ page }) => {
-  await page.goto('https://sfworkplaze.dataon.com/standard/hrm.time-and-attendance.attendance-request.leave-request');
+    await page.goto('https://sfworkplaze.dataon.com/');
+    await page.waitForTimeout(5000);
+    await expect(page).toHaveTitle(/Human Capital Management System/);
+    await page.getByRole('textbox', { name: '* Username' }).fill('gordon');
+    await page.getByRole('textbox', { name: '* Password' }).fill('password123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForTimeout(5000);
+    await expect(page).toHaveURL('https://sfworkplaze.dataon.com/standard/home');
+    await page.waitForTimeout(5000);
+    await page.goto('https://sfworkplaze.dataon.com/standard/hrm.time-and-attendance.attendance-request.leave-request');
 });
