2933c17e5f2644e4293af0118ce14ccf8e387012
Angga <anggatrianto180@gmail.com>
2025-07-04 14:55:31 +0700

    update git action use email

---
+++ tests/Reimbursment/Claim/AddClaimRequest.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('Record Attendance', async ({ page }) => {
+    await page.goto('https://sfworkplaze.dataon.com/');
+    await page.locator('#userName').fill('gordon');
+    await page.locator('#password').fill('password123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('[class="ant-btn ant-btn-primary"]').click();
+    await page.locator('[class="ant-typography"]');
+});
