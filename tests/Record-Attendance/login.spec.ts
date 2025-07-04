b49aa904921cf1f146ddc0e8fb7fa288ffcd3e1e
Angga <anggatrianto180@gmail.com>
2025-07-04 15:45:46 +0700

    Update Report TC Login

--- tests/Record-Attendance/login.spec.ts
+++ tests/Record-Attendance/login.spec.ts
@@ -1,9 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test('TC-01-Login', async ({page}) => {
+
+    await test.step('Langkah 1: Membuka halaman login', async () => {
     await page.goto('https://sfworkplaze.dataon.com/');
+  });
+
+  await test.step('Langkah 2: Mengisi username', async () => {
     await page.locator('#userName').fill('gordon');
+  });
+
+  await test.step('Langkah 3: Mengisi password', async () => {
     await page.locator('#password').fill('password123');
+  });
+
+  await test.step('Langkah 4: Menekan tombol Login', async () => {
     await page.getByRole('button', { name: 'Login' }).click();
+  });
+
+  await test.step('Langkah 5: Memverifikasi berhasil login dan masuk ke dashboard', async () => {
     await expect(page).toHaveURL('https://sfworkplaze.dataon.com/standard/home');
+  });
 });;
